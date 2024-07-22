1c0448132ba4938d6167f85d20721e9555ec7118
kris3301 <krishankantt97@gmail.com>
2024-07-22 23:52:56 +0530

    added the snake game flow.

--- src/App.js
+++ src/App.js
@@ -10,46 +10,85 @@ const pixelCss_def = {
   backgroundColor: "#6666eb",
 };
 const pixelCss_new = { width: "10px", height: "10px", backgroundColor: "red" };
+const pixelCss_food = {
+  width: "10px",
+  height: "10px",
+  backgroundColor: "black",
+};
+const boxCount = 20;
+const pixelArr = Array.from(Array(boxCount).keys());
 
 function App() {
-  const boxCount = 20;
-  const pixelArr = Array.from(Array(boxCount).keys());
-
-  const [newColorArr, setNewColorArr] = useState(["0:1", "0:2", "0:3", "0:4"]);
+  const [snakeArr, setSnakeArr] = useState([]);
+  const [snakeLen, setSnakeLen] = useState(3);
+  const [foodAxis, setFoodAxis] = useState("5:5");
+  const [pathStr, setPathStr] = useState("RIGHT");
 
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      let tempArr = [];
+      let tempArr = [...snakeArr];
+      let tempSnakeLen = snakeLen;
+      let newHeadArr = (snakeArr[0] || "0:0").split(":");
+      let new_Y_idx = parseInt(newHeadArr[0]);
+      let new_X_idx = parseInt(newHeadArr[1]);
 
-      for (let i = 0; i < newColorArr.length; i++) {
-        let newVal = getNxtHead(newColorArr[i]);
-        tempArr.push(newVal);
+      if (pathStr === "RIGHT") {
+        new_X_idx = new_X_idx + 1;
+      } else if (pathStr === "LEFT") {
+        new_X_idx = new_X_idx - 1;
+      } else if (pathStr === "UP") {
+        new_Y_idx = new_Y_idx - 1;
+      } else if (pathStr === "DOWN") {
+        new_Y_idx = new_Y_idx + 1;
       }
 
+      new_X_idx =
+        new_X_idx < 0 ? boxCount - 1 : new_X_idx >= boxCount ? 0 : new_X_idx;
+      new_Y_idx =
+        new_Y_idx < 0 ? boxCount - 1 : new_Y_idx >= boxCount ? 0 : new_Y_idx;
+      const newHead = `${new_Y_idx.toString()}:${new_X_idx.toString()}`;
+
+      tempArr = [newHead, ...tempArr];
+
+      if (newHead === foodAxis) {
+        let min = 0;
+        let max = boxCount - 1;
+        tempSnakeLen = tempSnakeLen + 1;
+
+        let food_x = Math.floor(Math.random() * (max - min + 1) + min);
+        let food_y = Math.floor(Math.random() * (max - min + 1) + min);
+        const food_YX = `${food_y.toString()}:${food_x.toString()}`;
+        setFoodAxis(food_YX);
+      }
+
+      if (tempArr.length > tempSnakeLen) {
+        tempArr.pop();
+      }
+
+      setSnakeArr(tempArr);
+      setSnakeLen(tempSnakeLen);
       setCount((prev) => prev + 1);
-      setNewColorArr(tempArr);
-    }, 100);
+    }, 200);
 
     // if (count >= 99) {
     //   clearInterval(intervalId);
     // }
 
     return () => clearInterval(intervalId);
-  }, [count]);
+  }, [count, pathStr]);
 
-  const getNxtHead = (headStr) => {
-    const headArr = headStr.split(":");
-    let head_y = parseInt(headArr[0]);
-    let head_x = parseInt(headArr[1]);
+  const onBtnClick = (btnType) => {
+    if (
+      (["RIGHT", "LEFT"].includes(pathStr) &&
+        ["RIGHT", "LEFT"].includes(btnType)) ||
+      (["UP", "DOWN"].includes(pathStr) && ["UP", "DOWN"].includes(btnType))
+    ) {
+      return;
+    }
 
-    head_x = head_x === boxCount ? 0 : head_x + 1;
-    head_y =
-      head_y === boxCount ? 0 : head_x === boxCount ? head_y + 1 : head_y;
-    const newHead = `${head_y.toString()}:${head_x.toString()}`;
-
-    return newHead;
+    setPathStr(btnType);
   };
 
   return (
@@ -59,22 +98,29 @@ function App() {
           return (
             <div style={rowCss} key={yIdx}>
               {pixelArr.map((_, xIdx) => {
-                return (
-                  <div
-                    style={
-                      newColorArr.includes(
-                        `${yIdx.toString()}:${xIdx.toString()}`
-                      )
-                        ? pixelCss_new
-                        : pixelCss_def
-                    }
-                    key={xIdx}
-                  ></div>
-                );
+                let tempIdx = `${yIdx.toString()}:${xIdx.toString()}`;
+                const pixelColorObj = snakeArr.includes(tempIdx)
+                  ? pixelCss_new
+                  : tempIdx === foodAxis
+                  ? pixelCss_food
+                  : pixelCss_def;
+
+                return <div style={pixelColorObj} key={xIdx}></div>;
               })}
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <button onClick={() => onBtnClick("UP")}>UP</button>
+
+        <div>
+          <button onClick={() => onBtnClick("LEFT")}>LEFT</button>
+          <button onClick={() => onBtnClick("RIGHT")}>RIGHT</button>
+        </div>
+
+        <button onClick={() => onBtnClick("DOWN")}>DOWN</button>
       </div>
     </div>
   );
